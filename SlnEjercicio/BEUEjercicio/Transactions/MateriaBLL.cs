1e31af700048e4c427274657415690889435acd2
Alex Montalvo <alex.montalvo.p@gmail.com>
2020-08-03 20:48:32 -0500

    Back End Update

---
+++ SlnEjercicio/BEUEjercicio/Transactions/MateriaBLL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Transactions
+{
+    public class LibroBLL
+    {
+        public static void Create(tblLibro a)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.tblLibroes.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static tblLibro Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.tblLibroes.Find(id);
+        }
+
+        public static void Update(tblLibro Libro)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.tblLibroes.Attach(Libro);
+                        db.Entry(Libro).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        tblLibro Libro = db.tblLibroes.Find(id);
+                        db.Entry(Libro).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<tblLibro> List()
+        {
+            Entities db = new Entities();            
+            return db.tblLibroes.ToList();            
+        }
+        
+
+
+    }
+}
---
+++ SlnEjercicio/WebApiEscolastico/Controllers/LibroController.cs
@@ -0,0 +1,89 @@
+﻿using BEUEjercicio;
+using BEUEjercicio.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace WebApiEscolastico.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class LibroController : ApiController
+    {
+        public IHttpActionResult Post(tblLibro Libro)
+        {
+            try
+            {
+                LibroBLL.Create(Libro);
+                return Content(HttpStatusCode.Created, "Libro creado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<tblLibro> todos = LibroBLL.List();
+                return Content(HttpStatusCode.OK, todos);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Put(tblLibro Libro)
+        {
+            try
+            {
+                LibroBLL.Update(Libro);
+                return Content(HttpStatusCode.OK, "Libro actualizado correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                tblLibro result = LibroBLL.Get(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Content(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                LibroBLL.Delete(id);
+                return Ok("Libro eliminado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
